d3c0e560f293ce0240e552a94eb75ea9c6a8a3dc
Giovanna Andrade <130957052+giovannaba@users.noreply.github.com>
2023-11-09 12:11:45 -0300

    criando novas rotas

--- jogoteca.py
+++ jogoteca.py
@@ -6,14 +6,16 @@ class Jogo:
         self.categoria=categoria
         self.console=console
 
+
+jogo1 = Jogo('Tetris', 'Puzzle', 'Atari')
+jogo2 = Jogo('God of War', 'Rack n Slash', 'PS2')
+jogo3 = Jogo('Mortal Kombat', 'Luta', 'PS2')
+lista = [jogo1, jogo2, jogo3]
+
 app = Flask(__name__)
 
 @app.route('/inicio')
 def ola():
-    jogo1 = Jogo('Tetris', 'Puzzle', 'Atari')
-    jogo2 = Jogo('God of War', 'Rack n Slash', 'PS2')
-    jogo3 = Jogo('Mortal Kombat', 'Luta', 'PS2')
-    lista = [jogo1, jogo2, jogo3]
     return render_template('lista.html', titulo='Jogos', jogos=lista)
 
 @app.route('/novo')
@@ -25,6 +27,9 @@ def criar():
     nome = request.form['nome']
     categoria = request.form['categoria']
     console = request.form['console']
+    jogo = Jogo(nome, categoria, console)
+    lista.append(jogo)
+    return render_template('lista.html', titulo='Jogos', jogos=lista)
 
 
 app.run()
